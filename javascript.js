54ed1bd019ca59851222ca56efbb93165bd13214
Bola-Kadiri <bolajimoh25@gmail.com>
2021-07-13 08:13:27 -0700

    My javascript

---
+++ javascript.js
@@ -0,0 +1,40 @@
+//Number 1
+let array1=[20, 46, 90, 80, 120];
+let array2=[2, 2, 5, 4, 2];
+let array3=[(array1[0/array2[0]]), (array1[1]/array2[1]), (array1[2]/array2[2]), (array1[3]/array2[3]), (array1[4]/array2[4])];
+ console.log(array3);
+ 
+ //Number 2
+ var arr =[[1, 2, 3],[4, 5, 6],7,8,9,[[10, 11, 12, [13, 4, 112]], 136, 14],];
+//print out these numbers (10,8,13,136 and 4)
+//from the array provided above
+
+var arr =[[1, 2, 3],[4, 5, 6],7,8,9,[[10, 11, 12, [13, 4, 112]], 136, 14],];
+var newArr=[(arr[5][0][0]), (arr[3]), (arr[5][0][3][0]), (arr[5][1]), (arr[5][0][3][1])];
+console.log(newArr);
+
+//Number 3
+// *I am a ______ developer at ________ and i love ______________
+var sentence = ["javascript", "web", "Capacity Bay"];
+var sen4= "I am _____ developer at ______ and I love _____";
+
+var complete= "I am  "+ sentence[1] + " developer at " + sentence[2] + " and  I love " + sentence[0] + " " ;
+
+
+//Question 4 : Use the bracket notation to print out 4th to the last character of "Generalization"
+var mySpeech="Generalization"; 
+mySpeech=mySpeech[mySpeech.length-4];
+console.log(mySpeech);
+
+//Number 5
+
+var fruits=["orange", "apple", "guava", "lemon"];
+fruits.unshift("pea", "banana");
+console.log(fruits);
+
+var fruits=["orange", "apple", "guava", "lemon"];
+fruits.push("Avocado", "mango");
+console.log(fruits);
+
+
+
